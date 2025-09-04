65c9a5e7cef48e032212283983678f493741ec39
Nasser <nparooei@outlook.com>
2025-09-04 16:36:39 +0200

    feat: making node

--- el/kamakazi-full-node/entrypoint/start-el.sh
+++ el/kamakazi-full-node/entrypoint/start-el.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 exec /nethermind/Nethermind.Runner \
   --datadir /opt/el/data \
-  --config /opt/el/config/nm2.cfg \
+  --config /opt/el/config/nm3.cfg \
   --Init.ChainSpecPath /opt/el/config/genesis.json \
   --Init.StaticNodesPath /opt/el/config/static-nodes.json \
   --Init.DiagnosticMode None
